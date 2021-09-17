e962bc959614f044cae98cc3d656b5d289d940a8
Diego Gabriel León Peña <diegabo2008@hotmail.com>
2021-09-17 17:50:44 -0400

    Control de errores en consumidor.

--- LCMDB.IISExtractor.ServicioEjecutor/ConsumidoresMQ/IISExtractorConsumidor.cs
+++ LCMDB.IISExtractor.ServicioEjecutor/ConsumidoresMQ/IISExtractorConsumidor.cs
@@ -2,7 +2,9 @@
 using LCMDB.BD.Contextos.LCMDB.Modelos.v1_0;
 using LCMDB.IISExtractor.BD.Contextos;
 using LCMDB.SolucionesAuxiliares.IISExtractor;
+using LCode.NETCore.Base._5._0.Logs;
 using MassTransit;
+using System;
 using System.Threading.Tasks;
 
 namespace LCMDB.IISExtractor.ServicioEjecutor.ConsumidoresMQ
@@ -12,27 +14,34 @@ namespace LCMDB.IISExtractor.ServicioEjecutor.ConsumidoresMQ
         private readonly Contexto _context = new Contexto();
         public async Task Consume(ConsumeContext<Servidor> context)
         {
-            Servidor servidor = context.Message;
-            RegistrarIIS registrarIIS = new RegistrarIIS(servidor.IP);
-            var Pools=registrarIIS.ObtenerAplicacionPools();
-            foreach(var pool in Pools)
+            try
             {
-                pool.IP=servidor.IP;
-                _context.GrupoAplicaciones.Add(pool);
-                await _context.SaveChangesAsync();
-            }
-            var SitiosWeb= registrarIIS.ObtenerSitiosWeb();
-            foreach(var Sitio in SitiosWeb)
-            {
-                Sitio.IdAppPool = Pools.Find(x => x.Nombre == Sitio.NombrePool).IdAppPool;
-                Sitio.IP=servidor.IP;
-                foreach(var app in Sitio.Aplicaciones)
+                Servidor servidor = context.Message;
+                RegistrarIIS registrarIIS = new RegistrarIIS(servidor.IP);
+                var Pools = registrarIIS.ObtenerAplicacionPools();
+                foreach (var pool in Pools)
                 {
-                    app.IP=servidor.IP;
-                    app.IdAppPool = Pools.Find(x => x.Nombre == app.NombrePool).IdAppPool;
+                    pool.IP = servidor.IP;
+                    _context.GrupoAplicaciones.Add(pool);
+                    await _context.SaveChangesAsync();
                 }
-                _context.SitiosWeb.Add(Sitio);
-                await _context.SaveChangesAsync();
+                var SitiosWeb = registrarIIS.ObtenerSitiosWeb();
+                foreach (var Sitio in SitiosWeb)
+                {
+                    Sitio.IdAppPool = Pools.Find(x => x.Nombre == Sitio.NombrePool).IdAppPool;
+                    Sitio.IP = servidor.IP;
+                    foreach (var app in Sitio.Aplicaciones)
+                    {
+                        app.IP = servidor.IP;
+                        app.IdAppPool = Pools.Find(x => x.Nombre == app.NombrePool).IdAppPool;
+                    }
+                    _context.SitiosWeb.Add(Sitio);
+                    await _context.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                Evento.Error(ex);
             }
         }
     }
